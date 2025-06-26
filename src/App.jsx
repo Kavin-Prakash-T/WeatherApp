5b1b3f4a5dfb27bfd0da15da6c09e89a88a802a8
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-06-26 19:32:42 +0530

    imported resources

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {useState} from 'react';
+import './App.css';
+
+import {searchIcon} from './assets/search.png';  
+import {clearIcon} from './assets/clear.png';  
+import {cloudIcon} from './assets/cloud.png';  
+import {drizzleIcon} from './assets/drizzle.png';  
+import {humidityIcon} from './assets/humidity.png';  
+import {rainIcon} from './assets/rain.png';  
+import {snowIcon} from './assets/snow.png';  
+import {windIcon} from './assets/wind.png';  
+
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
+
